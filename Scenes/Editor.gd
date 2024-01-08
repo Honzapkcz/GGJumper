fcdad777fe43729682e625a858f2231a43c75e2d
Honzapkcz <honzapkc@gmail.com>
2024-01-08 15:24:32 +0100

    Changed Type enum to Built-in Variant.Type enum

--- Scenes/Editor.gd
+++ Scenes/Editor.gd
@@ -13,6 +13,47 @@ enum O {
 	VC2 = 24, VC3 = 25, VC4 = 26, ARR = 27,
 	DCT = 28, OBJ = 48, SCN = 49, TXR = 50,
 }
+enum {
+	TYPE_NIL = 0,
+	TYPE_BOOL = 1,
+	TYPE_INT = 2,
+	TYPE_FLOAT = 3,
+	TYPE_STRING = 4,
+	TYPE_VECTOR2 = 5,
+	TYPE_VECTOR2I = 6,
+	TYPE_RECT2 = 7,
+	TYPE_RECT2I = 8,
+	TYPE_VECTOR3 = 9,
+	TYPE_VECTOR3I = 10,
+	TYPE_TRANSFORM2D = 11,
+	TYPE_VECTOR4 = 12,
+	TYPE_VECTOR4I = 13,
+	TYPE_PLANE = 14,
+	TYPE_QUATERNION = 15,
+	TYPE_AABB = 16,
+	TYPE_BASIS = 17,
+	TYPE_TRANSFORM3D = 18,
+	TYPE_PROJECTION = 19,
+	TYPE_COLOR = 20,
+	TYPE_STRING_NAME = 21,
+	TYPE_NODE_PATH = 22,
+	TYPE_RID = 23,
+	TYPE_OBJECT = 24,
+	TYPE_CALLABLE = 25,
+	TYPE_SIGNAL = 26,
+	TYPE_DICTIONARY = 27,
+	TYPE_ARRAY = 28,
+	TYPE_PACKED_BYTE_ARRAY = 29,
+	TYPE_PACKED_INT32_ARRAY = 30,
+	TYPE_PACKED_INT64_ARRAY = 31,
+	TYPE_PACKED_FLOAT32_ARRAY = 32,
+	TYPE_PACKED_FLOAT64_ARRAY = 33,
+	TYPE_PACKED_STRING_ARRAY = 34,
+	TYPE_PACKED_VECTOR2_ARRAY = 35,
+	TYPE_PACKED_VECTOR3_ARRAY = 36,
+	TYPE_PACKED_COLOR_ARRAY = 37,
+	TYPE_MAX = 38,
+}
 
 @onready var graphEdit: GraphEdit = $LSplit/RSplit/VSplit/GraphEdit
 @onready var world: Node2D = $LSplit/RSplit/VSplit/VPort/SVPort/World
@@ -35,6 +76,7 @@ func _ready():
 	world.in_editor = true
 	camera.make_current()
 	l_panel_size = lpanel.size.x
+	
 	#lpanel.custom_minimum_size.x = lpanel.size.x
 	
 	var butt: MenuButton = MenuButton.new()
@@ -67,6 +109,9 @@ func on_textedit_popup(value: String):
 
 func on_textedit_popup_update(value: String, node: Control):
 	node.text = value
+	
+func update_properties(object: Object):
+	object
 
 func _on_graph_edit_delete_nodes_request(nodes: Array[StringName]):
 	for i in nodes:
