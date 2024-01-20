17ef2475c55392548ac38a3cc4bda8218b27d92c
Honzapkcz <honzapkc@gmail.com>
2024-01-20 16:58:54 +0100

    Fixed ColorRect not scaling

--- Scenes/Transition.gd
+++ Scenes/Transition.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 
 @export var speed: float
 
+func _ready():
+	get_tree().root.size_changed.connect(root_changed)
+
 func change_scene(path: String):
 	$ColorRect.position = Vector2(DisplayServer.window_get_size().x, 0)
 	$ColorRect.visible = true
@@ -12,3 +15,6 @@ func change_scene(path: String):
 	#tween.tween_property($ColorRect, "position:x", 0, 0)
 	tween.tween_property($ColorRect, "position:x", -$ColorRect.size.x, speed).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property($ColorRect, "visible", false, 0)
+
+func root_changed():
+	$ColorRect.custom_minimum_size = get_tree().root.size
