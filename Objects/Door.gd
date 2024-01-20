fc2301b671f656cad807919a5fa0edb328ea4350
Honzapkcz <honzapkc@gmail.com>
2024-01-20 16:58:12 +0100

    Fixed doors not opening properly

--- Objects/Door.gd
+++ Objects/Door.gd
@@ -5,21 +5,29 @@ class_name Door
 @export var state: State
 @export var object_id: String
 @export var object_group: String
+@export var item_id: String
 
 enum State {
-	OPEN = 0,
-	CLOSE = 1,
+	OPEN = 1,
+	CLOSE = 0,
 }
 
 func _ready():
 	Global.animate.connect(on_animate)
 	Global.latch.connect(on_latch)
+	Global.pick.connect(on_pick)
 
 func on_animate(clock: int):
-	get_tree().create_tween().tween_property($Sprite2D, "modulate", Color(1, 1, 1, 1) if state else Color(1, 1, 1, 0), 1.0)
+	get_tree().create_tween().tween_property($Sprite2D, "modulate", Color(1, 1, 1, 0) if state else Color(1, 1, 1, 1), 1.0)
 
 func on_latch(on: bool, id: String, group: String):
 	if id != object_id and group != object_group:
 		return
-	state = 0 if on else 1
-
+	state = 1 if on else 0
+	set_collision_layer_value(2, not bool(state))
+	
+func on_pick(item: String):
+	if item != item_id:
+		return
+	state = abs(state - 1)
+	set_collision_layer_value(2, not bool(state))
