0c6ac3880d1bd1f00a857ac121d01466b2841ae7
Honzapkcz <honzapkc@gmail.com>
2024-01-24 18:09:16 +0100

    Buttons now can detect RigidBodies (boxes)

--- Objects/Button.gd
+++ Objects/Button.gd
@@ -9,14 +9,14 @@ func _ready():
 	Global.animate.connect(on_animate)
 
 func _on_body_entered(body: Node2D):
-	if not body is Player:
+	if not (body is Player or body is RigidBody2D):
 		return
 	state = true
 	Global.latch.emit(true, object_id, object_group)
 
 
 func _on_body_exited(body: Node2D):
-	if not body is Player:
+	if not (body is Player or body is RigidBody2D):
 		return
 	state = false
 	Global.latch.emit(false, object_id, object_group)
