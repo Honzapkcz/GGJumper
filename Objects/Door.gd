90582d38862eaca628965fa98359ec48f2ccfb38
Honzapkcz <honzapkc@gmail.com>
2024-01-24 20:04:21 +0100

    Fixed bug that Doors interacted with empty group

--- Objects/Door.gd
+++ Objects/Door.gd
@@ -21,7 +21,7 @@ func on_animate(clock: int):
 	get_tree().create_tween().tween_property($Sprite2D, "modulate", Color(1, 1, 1, 0) if state else Color(1, 1, 1, 1), Global.anim_speed)
 
 func on_latch(on: bool, id: String, group: String):
-	if id != object_id and group != object_group:
+	if id != object_id and (group != object_group or object_group.is_empty()):
 		return
 	state = 1 if on else 0
 	set_collision_layer_value(2, not bool(state))
