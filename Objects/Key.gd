1ab790977e34dbd56d93dfe10fd4be268455e7e2
Honzapkcz <honzapkc@gmail.com>
2024-01-24 18:48:47 +0100

    Key wiggles two times slower

--- Objects/Key.gd
+++ Objects/Key.gd
@@ -9,7 +9,9 @@ func _ready():
 	Global.animate.connect(on_animate)
 	
 func on_animate(clock: int):
-	get_tree().create_tween().tween_property($Sprite2D, "position:y", wiggle if clock % 2 else -wiggle, Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
+	if clock % 2 == 1:
+		return
+	get_tree().create_tween().tween_property($Sprite2D, "position:y", wiggle if clock % 4 else -wiggle, Global.anim_speed * 2).set_trans(Tween.TRANS_CUBIC)
 
 
 func _on_body_entered(body: Node2D):
