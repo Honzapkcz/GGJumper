a7dd5e0b4682e9e1b8f805d7ba52852013b5c918
Honzapkcz <honzapkc@gmail.com>
2024-01-25 14:45:04 +0100

    Added proccesing var to lock Transition during scene_change()

--- Scenes/Transition.gd
+++ Scenes/Transition.gd
@@ -2,10 +2,15 @@ extends CanvasLayer
 
 @export var speed: float
 
+var proccesing: bool = false
+
 func _ready():
 	get_tree().root.size_changed.connect(root_changed)
 
 func change_scene(path: String):
+	if proccesing:
+		return
+	proccesing = true
 	$ColorRect.position = Vector2(DisplayServer.window_get_size().x, 0)
 	$ColorRect.visible = true
 	var tween = get_tree().create_tween()
@@ -15,6 +20,7 @@ func change_scene(path: String):
 	#tween.tween_property($ColorRect, "position:x", 0, 0)
 	tween.tween_property($ColorRect, "position:x", -$ColorRect.size.x, speed).set_trans(Tween.TRANS_CUBIC)
 	tween.tween_property($ColorRect, "visible", false, 0)
+	tween.tween_property(self, "proccesing", false, 0)
 
 func root_changed():
 	$ColorRect.custom_minimum_size = get_tree().root.size
