fb8f518edac4b6441758b90747b5534fca7d7e13
Honzapkcz <honzapkc@gmail.com>
2024-01-26 19:28:07 +0100

    Repaired Button

--- Objects/Button.gd
+++ Objects/Button.gd
@@ -4,7 +4,7 @@ extends Area2D
 @export var object_group: String
 
 var state: bool
-var bodies: int
+var bodies: int = 0
 
 func _ready():
 	Global.animate.connect(on_animate)
@@ -23,7 +23,7 @@ func _on_body_exited(body: Node2D):
 	bodies -= 1
 	if not bodies:
 		state = false
-	Global.latch.emit(false, object_id, object_group)
+		Global.latch.emit(false, object_id, object_group)
 
 func on_animate(clock: int):
 	get_tree().create_tween().tween_property($Sprite2D, "position:y", -16 - 16 * int(not state), Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
