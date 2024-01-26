7e2923af3fc7ecb5555027410d975e44fba740eb
Honzapkcz <honzapkc@gmail.com>
2024-01-26 18:42:53 +0100

    Rewrote script

--- Objects/Lever.gd
+++ Objects/Lever.gd
@@ -4,7 +4,7 @@ extends Area2D
 @export var object_group: String
 
 var state: bool
-var bodies: int
+var can_latch: bool
 
 func _ready():
 	Global.animate.connect(on_animate)
@@ -12,18 +12,22 @@ func _ready():
 func _on_body_entered(body: Node2D):
 	if not (body is Player or body is RigidBody2D):
 		return
-	bodies += 1
-	state = true
+	can_latch = true
 	Global.latch.emit(true, object_id, object_group)
 
 
 func _on_body_exited(body: Node2D):
-	if not (body is Player or body is RigidBody2D):
+	if not body is Player:
 		return
-	bodies -= 1
-	if not bodies:
-		state = false
+	can_latch = false
 	Global.latch.emit(false, object_id, object_group)
 
+func _input(event: InputEvent):
+	if not event is InputEventKey:
+		return
+	if event.keycode != KEY_E:
+		return
+	state = not state
+
 func on_animate(clock: int):
-	get_tree().create_tween().tween_property($Sprite2D, "position:y", -16 - 16 * int(not state), Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
+	get_tree().create_tween().tween_property($Line2D, "rotation_degrees", 30 if state else -30 , Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
