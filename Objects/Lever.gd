810ac8170e8877927eea222e64a760c961117acd
Honzapkcz <honzapkc@gmail.com>
2024-01-26 19:45:06 +0100

    Minor Fixes

--- Objects/Lever.gd
+++ Objects/Lever.gd
@@ -13,21 +13,22 @@ func _on_body_entered(body: Node2D):
 	if not (body is Player or body is RigidBody2D):
 		return
 	can_latch = true
-	Global.latch.emit(true, object_id, object_group)
-
 
 func _on_body_exited(body: Node2D):
 	if not body is Player:
 		return
 	can_latch = false
-	Global.latch.emit(false, object_id, object_group)
+
 
 func _input(event: InputEvent):
 	if not event is InputEventKey:
 		return
+	if event.pressed:
+		return
 	if event.keycode != KEY_E:
 		return
 	state = not state
+	Global.latch.emit(state, object_id, object_group)
 
 func on_animate(clock: int):
 	get_tree().create_tween().tween_property($Line2D, "rotation_degrees", 30 if state else -30 , Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
