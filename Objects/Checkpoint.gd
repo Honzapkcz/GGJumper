2f4e325ea4e278c9176b79898ed405e5ac546102
Honzapkcz <honzapkc@gmail.com>
2024-11-27 23:04:44 +0100

    Dump changes for today

--- Objects/Checkpoint.gd
+++ Objects/Checkpoint.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
 @export var level = 0
+var visited: bool
 
 func _on_body_entered(body):
-	if body is Player:
-		Global.checkpoint.emit(position, level)
+	if not body is Player:
+		return
+	Global.checkpoint.emit(position, level)
+	if visited:
+		return
+	visited = true
+	get_tree().create_tween().tween_property($Flag, "position:y", -8, 0.5).set_trans(Tween.TRANS_CUBIC)
+	get_tree().create_tween().tween_property($Flag, "color", Color8(192, 192, 192), 0.5).set_trans(Tween.TRANS_CUBIC)
