9813873f5a2c2e5b858f227acc6c8295542282ae
Honzapkcz <honzapkc@gmail.com>
2025-01-17 20:39:48 +0100

    Fix bugs and method latch(on) to change state

--- Objects/Lever.gd
+++ Objects/Lever.gd
@@ -7,13 +7,10 @@ var state: bool
 var can_latch: bool
 
 func _ready() -> void:
-	state = start_state
-	animate()
-	await get_tree().process_frame
-	Global.latch.emit(state, object_id)
+	latch(start_state)
 
 func _on_body_entered(body: Node2D):
-	if not (body is Player or body is RigidBody2D):
+	if not body is Player:
 		return
 	can_latch = true
 
@@ -21,7 +18,6 @@ func _on_body_exited(body: Node2D):
 	if not body is Player:
 		return
 	can_latch = false
-
 
 func _input(event: InputEvent):
 	if not can_latch:
@@ -32,9 +28,9 @@ func _input(event: InputEvent):
 		return
 	if event.keycode != KEY_E:
 		return
-	state = not state
-	Global.latch.emit(state, object_id)
-	animate()
+	latch(not state)
 
-func animate():
+func latch(on: bool):
+	state = on
+	Global.latch.emit(state, object_id)
 	get_tree().create_tween().tween_property($Line2D, "rotation_degrees", 30 if state else -30 , Global.anim_speed).set_trans(Tween.TRANS_CUBIC)
