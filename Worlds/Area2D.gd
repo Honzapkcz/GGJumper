8a3232e5e03ec3de05351c2055b0eec3ad662194
Honzapkcz <honzapkc@gmail.com>
2025-01-17 20:38:08 +0100

    Add in newly created levels and add option for speedrun mode

--- Worlds/Area2D.gd
+++ Worlds/Area2D.gd
@@ -1,5 +1,25 @@
 extends Area2D
 
+var speedrun: bool
+
+func _ready() -> void:
+	await get_tree().physics_frame
+	if not Global.timer_paused:
+		$"../Lever".latch(true)
+		speedrun = true
+	Global.timer_paused = true
+	await get_tree().physics_frame
+	Global.latch.connect(on_latch)
+
+func _exit_tree() -> void:
+	Global.timer_paused = not speedrun
+	Global.timer = 0
+
+func on_latch(on: bool, id: String):
+	if not id == "speedrun":
+		return
+	speedrun = on
+	Global.timer = 0.0001 if on else 0
 
 func _on_body_entered(body):
 	if body is Player:
